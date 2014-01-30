417ffe45e392d1e57e7cacab69b22b857cffd347
Dig <dig.s.vargas@gmail.com>
2014-01-30 11:27:19 -0800

    Draws triangle ship and rotates with movement.

--- ship.js
+++ ship.js
@@ -11,6 +11,84 @@
 
   Ship.inherits(Asteroids.MovingObject);
 
+  Ship.prototype.draw = function(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+
+    // ctx.arc(
+    //   this.pos[0],
+    //   this.pos[1],
+    //   this.radius,
+    //   0,
+    //   2 * Math.PI,
+    //   false
+    // );
+
+    var line1x = this.pos[0]-15;
+    var line1y = this.pos[1]+35;
+    var line2x = this.pos[0]+15;
+    var line2y = this.pos[1]+35;
+
+    if ((this.dir[0] == -1) && (this.dir[1] == -1)) {
+      line1x = this.pos[0]+35;
+      line1y = this.pos[1]+15;
+      line2x = this.pos[0]+15;
+      line2y = this.pos[1]+35;
+    }
+
+    if ((this.dir[0] == -1) && (this.dir[1] == 0)) {
+      line1x = this.pos[0]+35;
+      line1y = this.pos[1]-15;
+      line2x = this.pos[0]+35;
+      line2y = this.pos[1]+15;
+    }
+
+    if ((this.dir[0] == -1) && (this.dir[1] == 1)) {
+      line1x = this.pos[0]+35;
+      line1y = this.pos[1]-15;
+      line2x = this.pos[0]+15;
+      line2y = this.pos[1]-35;
+    }
+
+    if ((this.dir[0] == 0) && (this.dir[1] == 1)) {
+      line1x = this.pos[0]+15;
+      line1y = this.pos[1]-35;
+      line2x = this.pos[0]-15;
+      line2y = this.pos[1]-35;
+    }
+
+    if ((this.dir[0] == 1) && (this.dir[1] == 1)) {
+      line1x = this.pos[0]-35;
+      line1y = this.pos[1]-15;
+      line2x = this.pos[0]-15;
+      line2y = this.pos[1]-35;
+    }
+
+    if ((this.dir[0] == 1) && (this.dir[1] == 0)) {
+      line1x = this.pos[0]-35;
+      line1y = this.pos[1]-15;
+      line2x = this.pos[0]-35;
+      line2y = this.pos[1]+15;
+    }
+
+    if ((this.dir[0] == 1) && (this.dir[1] == -1)) {
+      line1x = this.pos[0]-35;
+      line1y = this.pos[1]+15;
+      line2x = this.pos[0]-15;
+      line2y = this.pos[1]+35;
+    }
+
+    ctx.moveTo(this.pos[0],this.pos[1]);
+    ctx.lineTo(line1x,line1y);
+    ctx.lineTo(line2x,line2y);
+
+    ctx.fill();
+
+
+  };
+
+
+
   Ship.prototype.fireBullet = function(game){
     var ship = this;
     return new Asteroids.Bullet(ship.pos, 30, ship.dir, 3, "white", game);
